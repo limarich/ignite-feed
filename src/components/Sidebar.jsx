223af6d7a4d1056e373deccf0a28b0dcb2ba2e89
Richard <richard.esclima@gmail.com>
2022-11-05 09:20:01 -0300

    feat: add avatar to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,22 @@
 import styles from '../components/Sidebar.module.css'
 
+import {PencilLine} from "phosphor-react"
+
 export function Sidebar() {
 
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/limarich.png" />
+
                 <strong>Richard Lima</strong>
                 <span>Web developer</span>
             </div>
-            <footer>
-                <a href="#">Editar seu perfil</a>
+            <footer> 
+                <a href="#">
+                <PencilLine size={20}/>
+                    Editar seu perfil</a>
             </footer>
         </aside>
     )
